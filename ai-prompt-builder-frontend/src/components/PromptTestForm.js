393e045486bd80214513510cc00ad695f53c392c
modd3 <dimoreranx008@gmail.com>
2025-05-17 15:22:36 +0300

    Update PromptTestForm.js

--- ai-prompt-builder-frontend/src/components/PromptTestForm.js
+++ ai-prompt-builder-frontend/src/components/PromptTestForm.js
@@ -138,7 +138,6 @@ const PromptTestForm = ({ initialPrompt = null, onBack, onEdit }) => { // Added 
             const response = await fetch(process.env.REACT_APP_FRONTEND_API_URL + "/test-prompt", { // Replace with your backend URL
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json',
-                            'Access-Control-Allow-Origin': 'https://promptshare-0q9j.onrender.com',
                          },
                 body: JSON.stringify(testData),
             });
